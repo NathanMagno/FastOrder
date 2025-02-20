40777394ae4c4875d47b9caef2c28e41f8f2edba
nathan <nathan.consolo17@gmail.com>
2025-02-19 22:27:48 -0300

    feat: adicionando schadcn com alguns componentes

---
+++ fast-order/src/app/produtos/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const Produtos = () => {
+  return;
+  <>
+    <div className="p-5 border border-red-500 rounded-xl">
+      <h1 className="text-red-500">Produtos</h1>;<Button>Adicionar</Button>;
+      <Input placeholder="Pesquisar produto" />;
+    </div>
+    ;
+  </>;
+};
+
+export default Produtos;
